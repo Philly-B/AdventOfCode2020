9b69c5c3afcd579528e964c4431e468fd9b27490
Philipp Butz <philipp.butz@averbis.com>
2020-12-10 08:57:44 +0100

    Day10 formatting

--- src/days/Day10.java
+++ src/days/Day10.java
@@ -16,7 +16,7 @@ public class Day10 {
 				.map(Integer::parseInt)
 				.sorted()
 				.collect(Collectors.toList());
-		input.add(input.get(input.size() - 1)+3);
+		input.add(input.get(input.size() - 1) + 3);
 		input.add(0);
 		Collections.sort(input);
 
@@ -25,15 +25,15 @@ public class Day10 {
 	}
 
 
-
 	private static void solvePart1(List<Integer> input) {
 
 		int[] increments = new int[4];
 		for (int i = 1; i < input.size(); i++) {
-			increments[input.get(i) - input.get(i-1)]++;
+			increments[input.get(i) - input.get(i - 1)]++;
 		}
 		Helper.printResultPart1(String.valueOf(increments[1] * increments[3]));
 	}
+
 
 	private static void solvePart2(List<Integer> input) {
 
@@ -41,14 +41,14 @@ public class Day10 {
 		dp[0] = 1;
 		for (int i = 0; i < input.size(); i++) {
 			int currentNumber = input.get(i);
-			int pos = i-1;
-			while (pos >= 0 &&  currentNumber - input.get(pos) <= 3) {
+			int pos = i - 1;
+			while (pos >= 0 && currentNumber - input.get(pos) <= 3) {
 				dp[i] += dp[pos];
 				pos--;
 			}
 		}
 
-		Helper.printResultPart2(String.valueOf(dp[input.size()-1]));
+		Helper.printResultPart2(String.valueOf(dp[input.size() - 1]));
 	}
 
 }
