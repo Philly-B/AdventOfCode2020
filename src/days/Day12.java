b996f1d775709900b3d69348e3f206369f7cdf77
Philipp Butz <philipp.butz92@gmail.com>
2020-12-12 10:01:53 +0100

    Day12 refactoring

--- src/days/Day12.java
+++ src/days/Day12.java
@@ -1,122 +1,86 @@
 package days;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.function.Function;
 
-import datastructures.IntPair;
 import general.Helper;
 
 public class Day12 {
 
-	public static final char[] POS_MAP = new char[] { 'N', 'E', 'S', 'W' };
+	public static final int[][] movement = new int[][] {
+			{ 1, 0 }, // N
+			{ 0, 1 }, // E
+			{ -1, 0 }, // S
+			{ 0, -1 },// W
+			{0,0} // no move
+			 };
 
 
 	public static void main(String[] args) {
 
 		List<String> input = Helper.readFile("./data/days/day12p1.txt");
 
-		solvePart1(input);
-		solvePart2(input);
+		int resultPart1 = solve(input, true, 0, 1);
+		Helper.printResultPart1(String.valueOf(resultPart1));
+		int resultPart2 = solve(input, false, 1, 10);
+		Helper.printResultPart2(String.valueOf(resultPart2));
 	}
 
 
-	private static void solvePart1(List<String> input) {
+	private static int solve(List<String> input, boolean moveShip, int initialWayPointNS, int initialWayPointEW) {
 
-		char currentOrientation = 'E';
-		int numOfEast = 0;
-		int numOfNorth = 0;
+		int[] shipPos = new int[2]; // first is NS second is EW
+		int[] waypointPos = new int[] {initialWayPointNS, initialWayPointEW}; // first is NS second is EW
 
 		for (String op : input) {
 			char direction = op.charAt(0);
 			int dist = Integer.parseInt(op.substring(1));
 
-			if (direction == 'F') {
-				direction = currentOrientation;
-			}
+			int[] moveToDo = movement[4];
 
 			switch (direction) {
-				case 'E':
-					numOfEast += dist;
-					break;
-				case 'W':
-					numOfEast -= dist;
-					break;
 				case 'N':
-					numOfNorth += dist;
+					moveToDo = movement[0];
+					break;
+				case 'E':
+					moveToDo = movement[1];
 					break;
 				case 'S':
-					numOfNorth -= dist;
-					break;
-				case 'L':
-					dist = 360 - dist;
-				case 'R':
-					currentOrientation = changeOrientationByDegreeToRight(currentOrientation, dist);
-					break;
-			}
-		}
-		Helper.printResultPart1(String.valueOf(Math.abs(numOfEast) + Math.abs(numOfNorth)));
-	}
-
-
-	private static char changeOrientationByDegreeToRight(char currentOrientation, int dist) {
-
-		int movePositions = dist / 90;
-
-		for (int i = 0; i < POS_MAP.length; i++) {
-			if (POS_MAP[i] == currentOrientation) {
-				return POS_MAP[(i + movePositions) % POS_MAP.length];
-			}
-		}
-		throw new IllegalArgumentException("cant handle char " + currentOrientation);
-	}
-
-	private static void solvePart2(List<String> input) {
-
-		int shipNSPos = 0;
-		int shipEWPos = 0;
-
-		int waypointNSPos = 1;
-		int waypointEWPos = 10;
-
-		for (String op : input) {
-			char direction = op.charAt(0);
-			int dist = Integer.parseInt(op.substring(1));
-			switch (direction) {
-				case 'E':
-					waypointEWPos += dist;
+					moveToDo = movement[2];
 					break;
 				case 'W':
-					waypointEWPos -= dist;
-					break;
-				case 'N':
-					waypointNSPos += dist;
-					break;
-				case 'S':
-					waypointNSPos -= dist;
+					moveToDo = movement[3];
 					break;
 				case 'F':
-					shipNSPos += dist * waypointNSPos;
-					shipEWPos += dist * waypointEWPos;
+					movePointToWaypoint(shipPos, dist, waypointPos);
 					break;
 				case 'L':
 					dist = 360 - dist;
 				case 'R':
-					while (dist > 0) {
-							int tmp = waypointNSPos;
-							waypointNSPos = -1 * waypointEWPos;
-							waypointEWPos = tmp;
-						dist -= 90;
-					}
+					rotateWayPoint(waypointPos, dist);
 					break;
 			}
-			System.out.println("command " + op);
-			System.out.println("ship " + shipNSPos + " " + shipEWPos);
-			System.out.println("waypoint " + waypointNSPos + " " + waypointEWPos);
+			if (moveShip) {
+				movePointToWaypoint(shipPos, dist, moveToDo);
+			} else {
+				movePointToWaypoint(waypointPos, dist, moveToDo);
+			}
 		}
-		Helper.printResultPart2(String.valueOf( Math.abs(shipNSPos) + Math.abs(shipEWPos)));
+		return Math.abs(shipPos[0]) + Math.abs(shipPos[1]);
+	}
+
+	private static void movePointToWaypoint(int[] position, int moveDist, int[] moveBy) {
+		position[0] += moveDist * moveBy[0];
+		position[1] += moveDist * moveBy[1];
+	}
+
+	private static void rotateWayPoint(int[] waypointPos, int dist) {
+
+		while (dist > 0) {
+			int tmp = waypointPos[0];
+			waypointPos[0] = -1 * waypointPos[1];
+			waypointPos[1] = tmp;
+			dist -= 90;
+		}
 	}
 
 }
