326ee9024b190afc845b048330d4f3d53fdf5a43
Philipp Butz <philipp.butz92@gmail.com>
2020-12-14 14:32:46 +0100

    Added empty classes and input files for remaining days

---
+++ src/days/Day15.java
@@ -0,0 +1,20 @@
+package days;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import general.Helper;
+
+public class Day15 {
+
+	public static void main(String[] args) {
+
+		List<String> input = Helper.readFile("./data/days/day14p1.txt");
+	}
+
+
+}
