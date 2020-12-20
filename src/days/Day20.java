a86199f86654c391c044376ce7b3342d4f3fd246
Philipp Butz <philipp.butz@averbis.com>
2020-12-20 14:38:35 +0100

    Day20 part 2 wip correcting orientation

--- src/days/Day20.java
+++ src/days/Day20.java
@@ -23,8 +23,8 @@ public class Day20 {
 		input.add("");
 
 		Map<Integer, Image> imageIDToImage = readInput(input);
-		Map<String, List<Image>> borderToImages = createMapOfBorders(imageIDToImage);
-		Map<Integer, Set<Integer>> imageIdToNeigh = createMapOfNeighbors(borderToImages);
+		Map<String, List<ImageBorderPosTriple>> borderToImages = createMapOfBorders(imageIDToImage);
+		Map<Integer, Map<Integer, ImageMatch>> imageIdToNeigh = createMapOfNeighbors(borderToImages);
 
 		resolvePart1(imageIdToNeigh);
 
@@ -37,7 +37,7 @@ public class Day20 {
 	}
 
 
-	private static Image[][] buildActualGridOfImages(Map<Integer, Image> imageIDToImage, Map<Integer, Set<Integer>> imageIdToNeigh) {
+	private static Image[][] buildActualGridOfImages(Map<Integer, Image> imageIDToImage, Map<Integer, Map<Integer, ImageMatch>> imageIdToNeigh) {
 
 		int numberOfImagesPerRow = (int) Math.sqrt(imageIDToImage.size());
 		Image[][] gridOfImageIds = new Image[numberOfImagesPerRow][numberOfImagesPerRow];
@@ -50,20 +50,21 @@ public class Day20 {
 
 		Set<Integer> alreadyPlaced = new HashSet<>();
 		setTopLeftCorner(imageIDToImage, imageIdToNeigh, gridOfImageIds, upperLeftCornerImageId, alreadyPlaced);
-		setTopAndBottomBorder(imageIDToImage, imageIdToNeigh,  gridOfImageIds, corners, borders, alreadyPlaced);
-		setBottomAndRightBorder(imageIDToImage, imageIdToNeigh,  gridOfImageIds, corners, borders, alreadyPlaced);
-		setRestOfImage(imageIDToImage, imageIdToNeigh,  gridOfImageIds, midImage, alreadyPlaced);
+		setTopAndBottomBorder(imageIDToImage, imageIdToNeigh, gridOfImageIds, corners, borders, alreadyPlaced);
+		setBottomAndRightBorder(imageIDToImage, imageIdToNeigh, gridOfImageIds, corners, borders, alreadyPlaced);
+		setRestOfImage(imageIDToImage, imageIdToNeigh, gridOfImageIds, midImage, alreadyPlaced);
 
 		return gridOfImageIds;
 	}
 
-	private static void setRestOfImage(Map<Integer, Image> imageIDToImage, Map<Integer, Set<Integer>> imageIdToNeigh, Image[][] gridOfImageIds,
+
+	private static void setRestOfImage(Map<Integer, Image> imageIDToImage, Map<Integer, Map<Integer, ImageMatch>> imageIdToNeigh, Image[][] gridOfImageIds,
 			Set<Integer> midImage, Set<Integer> alreadyPlaced) {
 
-		for (int r1 = 1; r1 < gridOfImageIds.length -1; r1++) {
-			for (int c1 = 1; c1 < gridOfImageIds.length -1; c1++) {
-				Set<Integer> posNeigh = imageIdToNeigh.get(gridOfImageIds[r1 - 1][c1].imageId);
-				posNeigh.retainAll(imageIdToNeigh.get(gridOfImageIds[r1][c1 -1].imageId));
+		for (int r1 = 1; r1 < gridOfImageIds.length - 1; r1++) {
+			for (int c1 = 1; c1 < gridOfImageIds.length - 1; c1++) {
+				Set<Integer> posNeigh = imageIdToNeigh.get(gridOfImageIds[r1 - 1][c1].imageId).keySet();
+				posNeigh.retainAll(imageIdToNeigh.get(gridOfImageIds[r1][c1 - 1].imageId).keySet());
 				posNeigh.retainAll(midImage);
 				posNeigh.removeAll(alreadyPlaced);
 				gridOfImageIds[r1][c1] = imageIDToImage.get(posNeigh.iterator().next());
@@ -73,51 +74,79 @@ public class Day20 {
 	}
 
 
-	private static void setTopLeftCorner(Map<Integer, Image> imageIDToImage, Map<Integer, Set<Integer>> imageIdToNeigh, Image[][] gridOfImageIds, int upperLeftCornerImageId,
+	private static void setTopLeftCorner(Map<Integer, Image> imageIDToImage, Map<Integer, Map<Integer, ImageMatch>> imageIdToNeigh, Image[][] gridOfImageIds, int upperLeftCornerImageId,
 			Set<Integer> alreadyPlaced) {
 
 		gridOfImageIds[0][0] = imageIDToImage.get(upperLeftCornerImageId);
-		Integer[] neigh = imageIdToNeigh.get(gridOfImageIds[0][0].imageId).toArray(new Integer[0]);
+		Integer[] neigh = imageIdToNeigh.get(gridOfImageIds[0][0].imageId).keySet().toArray(new Integer[0]);
 		gridOfImageIds[1][0] = imageIDToImage.get(neigh[0]);
 		gridOfImageIds[0][1] = imageIDToImage.get(neigh[1]);
 		alreadyPlaced.add(upperLeftCornerImageId);
 		alreadyPlaced.add(neigh[0]);
 		alreadyPlaced.add(neigh[1]);
+
+		correctImageOrientation(imageIdToNeigh, gridOfImageIds, 0, 0);
+
 	}
 
 
-	private static void setBottomAndRightBorder(Map<Integer, Image> imageIDToImage, Map<Integer, Set<Integer>> imageIdToNeigh, Image[][] gridOfImageIds,
+	private static void correctImageOrientation(Map<Integer, Map<Integer, ImageMatch>> imageIdToNeigh, Image[][] gridOfImageIds, int currRow, int curCol) {
+
+		Map<Integer, ImageMatch> neighborsOfcurrent = imageIdToNeigh.get(gridOfImageIds[currRow][curCol].imageId);
+		ImageMatch imageMatch = neighborsOfcurrent.get(gridOfImageIds[currRow+1][curCol].imageId);
+		int posOneWithoutRev = imageMatch.posOne % 10;
+		int posTwoWithoutRev = imageMatch.posTwo % 10;
+
+		while (posTwoWithoutRev != posOneWithoutRev) {
+			gridOfImageIds[currRow][curCol].rotate90ToRight();
+			posOneWithoutRev = (posOneWithoutRev + 1) % 4;
+		}
+		if (posOneWithoutRev >= 10) {
+			gridOfImageIds[currRow][curCol].flipLeftRight();
+		}
+		imageMatch = neighborsOfcurrent.get(gridOfImageIds[curCol][currRow + 1].imageId);
+		posOneWithoutRev = imageMatch.posOne % 10;
+
+		if (posOneWithoutRev >= 10) {
+			gridOfImageIds[currRow][curCol].flipTopBottom();
+		}
+	}
+
+
+	private static void setBottomAndRightBorder(Map<Integer, Image> imageIDToImage, Map<Integer, Map<Integer, ImageMatch>> imageIdToNeigh, Image[][] gridOfImageIds,
 			Set<Integer> corners, Set<Integer> borders, Set<Integer> alreadyPlaced) {
 
 		for (int i = 1; i < gridOfImageIds.length; i++) {
-			Set<Integer> posNeigh = imageIdToNeigh.get(gridOfImageIds[gridOfImageIds.length -1][i-1].imageId);
+			Set<Integer> posNeigh = imageIdToNeigh.get(gridOfImageIds[gridOfImageIds.length - 1][i - 1].imageId).keySet();
 			posNeigh.removeAll(alreadyPlaced);
 			Integer actualNeigh = posNeigh.stream().filter(id -> borders.contains(id) || corners.contains(id)).findAny().get();
-			gridOfImageIds[gridOfImageIds.length -1][i] = imageIDToImage.get(actualNeigh);
+			gridOfImageIds[gridOfImageIds.length - 1][i] = imageIDToImage.get(actualNeigh);
 			alreadyPlaced.add(actualNeigh);
 
-			if (i == gridOfImageIds.length -1 ) continue;
+			if (i == gridOfImageIds.length - 1) {
+				continue;
+			}
 
-			posNeigh = imageIdToNeigh.get(gridOfImageIds[i-1][gridOfImageIds.length -1].imageId);
+			posNeigh = imageIdToNeigh.get(gridOfImageIds[i - 1][gridOfImageIds.length - 1].imageId).keySet();
 			posNeigh.removeAll(alreadyPlaced);
 			actualNeigh = posNeigh.stream().filter(id -> borders.contains(id) || corners.contains(id)).findAny().get();
-			gridOfImageIds[i][gridOfImageIds.length -1] = imageIDToImage.get(actualNeigh);
+			gridOfImageIds[i][gridOfImageIds.length - 1] = imageIDToImage.get(actualNeigh);
 			alreadyPlaced.add(actualNeigh);
 		}
 	}
 
 
-	private static void setTopAndBottomBorder(Map<Integer, Image> imageIDToImage, Map<Integer, Set<Integer>> imageIdToNeigh, Image[][] gridOfImageIds,
+	private static void setTopAndBottomBorder(Map<Integer, Image> imageIDToImage, Map<Integer, Map<Integer, ImageMatch>> imageIdToNeigh, Image[][] gridOfImageIds,
 			Set<Integer> corners, Set<Integer> borders, Set<Integer> alreadyPlaced) {
 
 		for (int i = 2; i < gridOfImageIds.length; i++) {
-			Set<Integer> posNeigh = imageIdToNeigh.get(gridOfImageIds[0][i - 1].imageId);
+			Set<Integer> posNeigh = imageIdToNeigh.get(gridOfImageIds[0][i - 1].imageId).keySet();
 			posNeigh.removeAll(alreadyPlaced);
 			Integer actualNeigh = posNeigh.stream().filter(id -> borders.contains(id) || corners.contains(id)).findAny().get();
 			gridOfImageIds[0][i] = imageIDToImage.get(actualNeigh);
 			alreadyPlaced.add(actualNeigh);
 
-			posNeigh = imageIdToNeigh.get(gridOfImageIds[i - 1][0].imageId);
+			posNeigh = imageIdToNeigh.get(gridOfImageIds[i - 1][0].imageId).keySet();
 			posNeigh.removeAll(alreadyPlaced);
 			actualNeigh = posNeigh.stream().filter(id -> borders.contains(id) || corners.contains(id)).findAny().get();
 			gridOfImageIds[i][0] = imageIDToImage.get(actualNeigh);
@@ -126,7 +155,7 @@ public class Day20 {
 	}
 
 
-	private static void resolvePart1(Map<Integer, Set<Integer>> imageIdToNeigh) {
+	private static void resolvePart1(Map<Integer, Map<Integer, ImageMatch>> imageIdToNeigh) {
 
 		long result = getAllImagesWithNumberOfNeigh(imageIdToNeigh, 2)
 				.reduce(1, (l1, l2) -> l1 * l2);
@@ -136,17 +165,18 @@ public class Day20 {
 
 
 	private static Set<Integer> getAllImagesWithNumberOfNeighAsIntSet(
-			Map<Integer, Set<Integer>> imageIdToNeigh,
+			Map<Integer, Map<Integer, ImageMatch>> imageIdToNeigh,
 			int numberOfNeigh) {
 
-		return getAllImagesWithNumberOfNeigh(imageIdToNeigh,numberOfNeigh)
+		return getAllImagesWithNumberOfNeigh(imageIdToNeigh, numberOfNeigh)
 				.boxed()
 				.map(Long::intValue)
 				.collect(Collectors.toSet());
 	}
 
+
 	private static LongStream getAllImagesWithNumberOfNeigh(
-			Map<Integer, Set<Integer>> imageIdToNeigh,
+			Map<Integer, Map<Integer, ImageMatch>> imageIdToNeigh,
 			int numberOfNeigh) {
 
 		return imageIdToNeigh
@@ -157,43 +187,47 @@ public class Day20 {
 	}
 
 
-	private static Map<Integer, Set<Integer>> createMapOfNeighbors(Map<String, List<Image>> borderToImages) {
+	private static Map<Integer, Map<Integer, ImageMatch>> createMapOfNeighbors(Map<String, List<ImageBorderPosTriple>> borderToImages) {
 
-		Map<Integer, Set<Integer>> imageIdToNeigh = new HashMap<>();
-		Iterator<Map.Entry<String, List<Image>>> iterator = borderToImages.entrySet().iterator();
+		Map<Integer, Map<Integer, ImageMatch>> matchOfTwoImages = new HashMap<>();
+
+		Iterator<Map.Entry<String, List<ImageBorderPosTriple>>> iterator = borderToImages.entrySet().iterator();
 		while (iterator.hasNext()) {
-			Map.Entry<String, List<Image>> next = iterator.next();
-			List<Image> neightbors = next.getValue();
-			if (neightbors.size() == 1) {
+			Map.Entry<String, List<ImageBorderPosTriple>> next = iterator.next();
+			List<ImageBorderPosTriple> neighbors = next.getValue();
+			if (neighbors.size() == 1) {
 				iterator.remove();
 				continue;
 			}
 
-			if (neightbors.size() != 2) {
+			if (neighbors.size() != 2) {
 				throw new IllegalArgumentException();
 			}
 
-			imageIdToNeigh.putIfAbsent(neightbors.get(0).imageId, new HashSet<>());
-			imageIdToNeigh.get(neightbors.get(0).imageId).add(neightbors.get(1).imageId);
-			imageIdToNeigh.putIfAbsent(neightbors.get(1).imageId, new HashSet<>());
-			imageIdToNeigh.get(neightbors.get(1).imageId).add(neightbors.get(0).imageId);
+			ImageBorderPosTriple first = neighbors.get(0);
+			ImageBorderPosTriple second = neighbors.get(1);
+
+			matchOfTwoImages.putIfAbsent(first.image.imageId, new HashMap<>());
+			matchOfTwoImages.get(first.image.imageId).put(second.image.imageId, new ImageMatch(first.image, second.image, first.pos, second.pos));
+
+			matchOfTwoImages.putIfAbsent(second.image.imageId, new HashMap<>());
+			matchOfTwoImages.get(second.image.imageId).put(first.image.imageId, new ImageMatch(second.image, first.image, second.pos, first.pos));
 		}
-		return imageIdToNeigh;
+		return matchOfTwoImages;
 	}
 
 
-	private static Map<String, List<Image>> createMapOfBorders(Map<Integer, Image> imageIDToImage) {
+	private static Map<String, List<ImageBorderPosTriple>> createMapOfBorders(Map<Integer, Image> imageIDToImage) {
 
-		Map<String, List<Image>> borderToImages = new HashMap<>();
+		Map<String, List<ImageBorderPosTriple>> borderToImages = new HashMap<>();
 		for (Image image : imageIDToImage.values()) {
-			for (String border : image.allBorders) {
-				borderToImages.putIfAbsent(border, new ArrayList<>());
-				borderToImages.get(border).add(image);
+			for (Map.Entry<Integer, String> posBorderPair : image.posToBorders.entrySet()) {
+				borderToImages.putIfAbsent(posBorderPair.getValue(), new ArrayList<>());
+				borderToImages.get(posBorderPair.getValue()).add(new ImageBorderPosTriple(image, posBorderPair.getKey(), posBorderPair.getValue()));
 			}
 		}
 		return borderToImages;
 	}
-
 
 	private static Map<Integer, Image> readInput(List<String> input) {
 
@@ -205,10 +239,10 @@ public class Day20 {
 		int currRow = 0;
 		for (String line : input) {
 			if (line.isEmpty() && currentImg != null) {
-				currentImg.allBorders.add(left.toString());
-				currentImg.allBorders.add(left.reverse().toString());
-				currentImg.allBorders.add(right.toString());
-				currentImg.allBorders.add(right.reverse().toString());
+				currentImg.posToBorders.put(3, left.toString());
+				currentImg.posToBorders.put(13, left.reverse().toString());
+				currentImg.posToBorders.put(1, right.toString());
+				currentImg.posToBorders.put(11, right.reverse().toString());
 				right = new StringBuilder();
 				left = new StringBuilder();
 				imageIDToImage.put(currentImg.imageId, currentImg);
@@ -217,10 +251,15 @@ public class Day20 {
 			} else if (line.startsWith("Tile")) {
 				currentImg = new Image(Integer.parseInt(line.substring(5, 9)), binImg);
 			} else {
-				if (currRow == 0 || currRow == IMAGE_SIZE - 1) {
-					currentImg.allBorders.add(line);
-					currentImg.allBorders.add(new StringBuilder(line).reverse().toString());
+				if (currRow == 0) {
+					currentImg.posToBorders.put(0, line);
+					currentImg.posToBorders.put(10, new StringBuilder(line).reverse().toString());
+
+				} else if (currRow == IMAGE_SIZE - 1) {
+					currentImg.posToBorders.put(2, line);
+					currentImg.posToBorders.put(12, new StringBuilder(line).reverse().toString());
 				}
+
 				char[] row = line.toCharArray();
 				for (int i = 0; i < row.length; i++) {
 					binImg[currRow][i] = row[i] == '#';
@@ -237,12 +276,42 @@ public class Day20 {
 		return imageIDToImage;
 	}
 
+	private static class ImageBorderPosTriple {
+		Image image;
+		int pos;
+		String border;
+
+		public ImageBorderPosTriple(Image image, int pos, String border) {
+
+			this.image = image;
+			this.pos = pos;
+			this.border = border;
+		}
+	}
+
+	private static class ImageMatch {
+		Image one;
+		Image two;
+		int posOne;
+		int posTwo;
+
+		public ImageMatch(Image one, Image two, int posOne, int posTwo) {
+
+			this.one = one;
+			this.two = two;
+			this.posOne = posOne;
+			this.posTwo = posTwo;
+		}
+	}
 
 	private static class Image {
 
 		int imageId;
 		private boolean[][] binImg;
-		Set<String> allBorders = new HashSet<>();
+
+		// pos = top right bottom left => 0 1 2 3
+		// rev pos = top right bottom left => 10 11 12 13
+		Map<Integer, String> posToBorders = new HashMap<>();
 
 
 		public Image(int imageId, boolean[][] binImg) {
