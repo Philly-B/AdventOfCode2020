b092fc5920b514255a6d959e07a42bdcaf3fd627
Philipp Butz <philipp.butz92@gmail.com>
2020-12-23 11:29:13 +0100

    Day23

--- src/days/Day23.java
+++ src/days/Day23.java
@@ -2,51 +2,79 @@ package days;
 
 import general.Helper;
 
-import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 public class Day23 {
 
+    public static final String TEST_INPUT = "389125467";
 
     public static void main(String[] args) {
 
-        runTestCase();
-
         String actualInput = "712643589";
         solveForPart1(actualInput);
-
-    }
-
-    private static void runTestCase() {
-        String testInput = "389125467";
-        String expectedResult = "67384529";
-        String testResult = executeMoves(testInput);
-        if (!expectedResult.equals(testResult)) {
-            throw new IllegalArgumentException("Test fails");
-        }
+        solveForPart2(actualInput);
     }
 
     private static void solveForPart1(String input) {
-        String result = executeMoves(input);
-        Helper.printResultPart1(result);
+        String expectedResult = "67384529";
+        Map<Integer, ListItem> listItems = playGame(TEST_INPUT, 9, 100);
+        String testResult = calcResultStartingFrom(listItems.get(1));
+        if (!expectedResult.equals(testResult)) {
+            throw new IllegalArgumentException("Test fails");
+        }
+        listItems = playGame(input, 9, 100);
+        String actualResult = calcResultStartingFrom(listItems.get(1));
+        Helper.printResultPart1(actualResult);
     }
 
-    private static String executeMoves(String input) {
-        ListItem[] numToListItem = new ListItem[10];
+    private static void solveForPart2(String input) {
+        long expectedResult = 149245887792l;
+        Map<Integer, ListItem> listItems = playGame(TEST_INPUT, 1_000_000, 10_000_000);
+        long testResult = ((long) listItems.get(1).next.value) * listItems.get(1).next.next.value;
+        if (expectedResult != testResult) {
+            throw new IllegalArgumentException("Test fails " + testResult + " should be " + expectedResult);
+        }
+        listItems = playGame(input, 1_000_000, 10_000_000);
+        long actualResult = ((long) listItems.get(1).next.value) * listItems.get(1).next.next.value;
+        Helper.printResultPart2(String.valueOf(actualResult));
+    }
+
+    private static Map<Integer, ListItem> playGame(String input, int fillUpUntil, int moves) {
+        Map<Integer, ListItem> numToListItem = new HashMap<>();
         ListItem first = readInput(input, numToListItem);
-        executeMoves(numToListItem, first);
-        return calcResultStartingFrom(numToListItem[1]);
+        fillUpTheCups(fillUpUntil, numToListItem, first);
+        playGame(numToListItem, first, moves);
+        return numToListItem;
     }
 
-    private static void executeMoves(ListItem[] numToListItem, ListItem first) {
+    private static void fillUpTheCups(int fillUpUntil, Map<Integer, ListItem> numToListItem, ListItem first) {
+        ListItem lastItem = first.next;
+        while (lastItem.next != first) {
+            lastItem = lastItem.next;
+        }
+
+        for (int i = numToListItem.size() + 1; i <= fillUpUntil; i++) {
+            ListItem nextItem = new ListItem(i);
+            numToListItem.put(i, nextItem);
+            lastItem.next = nextItem;
+            lastItem = nextItem;
+        }
+        lastItem.next = first;
+    }
+
+
+    private static void playGame(Map<Integer, ListItem> numToListItem, ListItem first, int moves) {
         ListItem current;
-        boolean[] pickUpVals = new boolean[10];
+        Set<Integer> pickUpVals = new HashSet<>();
         current = first;
-        for (int i = 0; i < 100; i++) {
-            Arrays.fill(pickUpVals, false);
+        for (int i = 0; i < moves; i++) {
             ListItem nextAfterPickUp = current;
             for (int j = 0; j < 3; j++) {
                 nextAfterPickUp = nextAfterPickUp.next;
-                pickUpVals[nextAfterPickUp.value] = true;
+                pickUpVals.add(nextAfterPickUp.value);
             }
 
             ListItem dest = findValidDestination(current, pickUpVals, numToListItem);
@@ -58,16 +86,17 @@ public class Day23 {
             nextAfterPickUp.next = destNext; // fix end of pickups
 
             current = current.next;
+            pickUpVals.clear();
         }
     }
 
-    private static ListItem readInput(String input, ListItem[] numToListItem) {
+    private static ListItem readInput(String input, Map<Integer, ListItem> numToListItem) {
         ListItem first = null;
         ListItem current = null;
         for (char c : input.toCharArray()) {
             int num = c - '0';
             ListItem next = new ListItem(num);
-            numToListItem[num] = next;
+            numToListItem.put(num, next);
             if (current != null)
                 current.next = next;
             current = next;
@@ -89,43 +118,17 @@ public class Day23 {
         return result.toString();
     }
 
-    private static ListItem findValidDestination(ListItem current, boolean[] pickUpVals, ListItem[] numToListItem) {
-        int destination = nextDestination(current.value);
-        boolean valid = false;
-        while (!valid) {
-            valid = true;
-            for (int d = 1; d < 10; d++) {
-                if (pickUpVals[d] && destination == d) {
-                    destination = nextDestination(destination);
-                    valid = false;
-                    break;
-                }
-            }
-        }
-        return numToListItem[destination];
+    private static ListItem findValidDestination(ListItem current, Set<Integer> pickUpVals, Map<Integer, ListItem> numToListItem) {
+        int destination = nextDestination(current.value, numToListItem);
+        while (pickUpVals.contains(destination)) destination = nextDestination(destination, numToListItem);
+        return numToListItem.get(destination);
     }
 
-    private static int nextDestination(int destination) {
+    private static int nextDestination(int destination, Map<Integer, ListItem> numToListItem) {
         int nextDest = destination - 1;
-        if (nextDest == 0) nextDest = 9;
+        if (nextDest == 0) nextDest = numToListItem.size();
         return nextDest;
     }
-
-
-    private static void printList(ListItem first) {
-
-        boolean[] seen = new boolean[10];
-        ListItem curr = first;
-        do {
-            seen[curr.value] = true;
-            System.out.print(curr.value + " ");
-            curr = curr.next;
-        } while (!seen[curr.value]);
-
-        System.out.println();
-
-    }
-
 
     private static class ListItem {
 
